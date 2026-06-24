05c955b40ecc02efebfa90d409bf3ffe12bdbeab
LukStar2018 <stanitz.lukas@gmail.com>
2026-06-24 11:30:29 +0200

    add attribute und funktoinen der boundary klassen

--- BoobyBank/src/main/java/com/example/boobybank/boundary/accounts/CurrentAccountDTO.java
+++ BoobyBank/src/main/java/com/example/boobybank/boundary/accounts/CurrentAccountDTO.java
@@ -1,4 +1,50 @@
 package com.example.boobybank.boundary.accounts;
 
-public class CurrentAccountDTO {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class CurrentAccountDTO extends AccountDTO {
+
+    @Min(0)
+    private Double overdraftLimit;
+    @Min(0)
+    @Max(100)
+    private Float overdraftInterestRate;
+
+
+    CurrentAccountDTO(
+            String iban,
+            double balance,
+            Double overdraftLimit,
+            Float overdraftInterestRate) {
+        super(iban, balance, "current");
+
+        this.overdraftLimit = overdraftLimit;
+        this.overdraftInterestRate = overdraftInterestRate;
+    }
+
+
+    public static CurrentAccountDTO newFrom(CurrentAccountDTO account) {
+        return new CurrentAccountDTO(
+                account.getIban(),
+                0.0,
+                account.getOverdraftLimit(),
+                account.getOverdraftInterestRate()
+        );
+    }
+
+
+    public static CurrentAccountDTO copyFrom(CurrentAccountDTO account) {
+        return new CurrentAccountDTO(
+                account.getIban(),
+                account.getBalance(),
+                account.getOverdraftLimit(),
+                account.getOverdraftInterestRate()
+        );
+    }
+
 }
--- BoobyBank/src/main/java/com/example/boobybank/control/shared/NotFoundException.java
+++ BoobyBank/src/main/java/com/example/boobybank/control/shared/NotFoundException.java
@@ -1,4 +1,7 @@
 package com.example.boobybank.control.shared;
 
-public class NotFoundException {
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
